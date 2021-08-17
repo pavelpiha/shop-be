fcba90a7c69c01efb6ce4d4ed4300b6de4238510
Pavel Piha <ppiha@infoblox.com>
2021-08-17 18:11:24 +0300

    feat: add task-5

---
+++ import-service/README.md
@@ -0,0 +1,8 @@
+# Serverless - AWS Node.js Typescript
+
+This project has been generated using the `aws-nodejs-typescript` template from the [Serverless framework](https://www.serverless.com/).
+
+This is backend for the Shop application it allows:
+
+1. Import product file from UI and save it on S3
+2. When imported files uploads to S3 it will parsed, applied to DB(not implemented yet) and move file to other folder
---
+++ import-service/src/functions/import-file/handler.ts
@@ -0,0 +1,33 @@
+import "source-map-support/register";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/apiGateway";
+import { formatJSONResponse } from "@libs/apiGateway";
+import { middyfy } from "@libs/lambda";
+import createHttpError from "http-errors";
+
+import schema from "./schema";
+
+const handler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
+  event
+) => {
+  try {
+    const fileName = event.queryStringParameters.name;
+    const getObjectParams = {
+      Bucket: "shop-csv-upload",
+      Key: `uploaded/${fileName}`,
+      ContentType: "text/csv",
+    };
+    const client = new S3Client({
+      region: "eu-west-1",
+    });
+    const command = new PutObjectCommand(getObjectParams);
+    const signedUrl = await getSignedUrl(client, command, { expiresIn: 60 });
+    return formatJSONResponse(200, signedUrl);
+  } catch (error) {
+    const response = new createHttpError.InternalServerError(error.message);
+    return formatJSONResponse(response.statusCode, response.message);
+  }
+};
+
+export const importProductsFile = middyfy(handler);
