53f727af35cba0f65e70187df48710d93c2f501a
Pavel Piha <ppiha@infoblox.com>
2021-08-29 13:00:13 +0300

    feat: task-6 SQS SNS

---
+++ import-service/src/service/import-file-service.ts
@@ -0,0 +1,128 @@
+import {
+  CopyObjectCommand,
+  DeleteObjectCommand,
+  GetObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { S3EventRecord } from "aws-lambda";
+import { validateSync, ValidationError } from "class-validator";
+import csvParser from "csv-parser";
+import createHttpError from "http-errors";
+import { Readable } from "stream";
+import { ProductDto } from "../dto/product.dto";
+import queueService from "./queue.service";
+
+const BUCKET_NAME = "shop-file-upload";
+const s3Client = new S3Client({
+  region: "eu-west-1",
+});
+
+export const handleImportedFiles = async (
+  records: S3EventRecord[]
+): Promise<any> => {
+  const promisesWithProducts = records.map((record) => {
+    return processImportedFile(record);
+  });
+  const productsArray = await Promise.all(promisesWithProducts);
+  const result = productsArray.flat();
+  console.log("$$$$$$$$$$$$$$$$$$$$$handleImportedFiles", result);
+  return result;
+  // try {
+  //   for (const record of records) {
+  //     return await processImportedFile(record);
+  //   }
+  // } catch (error) {
+  //   console.error("handleImportedFiles error was thrown", error);
+  //   throw error;
+};
+// };
+
+const processImportedFile = async (record: S3EventRecord) => {
+  try {
+    const name = record.s3.object.key;
+    const getObjectParams = {
+      Bucket: BUCKET_NAME,
+      Key: name,
+    };
+    const command = new GetObjectCommand(getObjectParams);
+    const item = await s3Client.send(command);
+    const products: ProductDto[] = await readStream(item.Body);
+
+    const validationErrors: ValidationError[][] | null =
+      validateProducts(products);
+
+    if (validationErrors) {
+      console.error(`Validation failed for ${name}`, validationErrors);
+      throw new createHttpError.NotAcceptable(
+        `Not valid values in imported file`
+      );
+    } else {
+      await moveFileToParsedFolder(record);
+      products.forEach((product: ProductDto) => {
+        queueService.addToQueue(product);
+      });
+      return products;
+    }
+  } catch (error) {
+    console.error("processImportedFile error was thrown", error);
+    throw error;
+  }
+};
+
+const moveFileToParsedFolder = async (record) => {
+  await copyFileToParsedFolder(record);
+  await deleteFromUploaded(record);
+};
+
+const readStream = (stream: Readable): Promise<any> => {
+  console.log("readStream!!!!!!!!!!!", stream);
+  const allRowsData = [];
+  return new Promise((resolve, reject) => {
+    stream
+      .pipe(csvParser())
+      .on("data", (row) => {
+        console.log("row", row);
+        allRowsData.push(row);
+      })
+      .on("end", () => {
+        resolve(allRowsData);
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
+};
+
+export const validateProducts = (
+  data: ProductDto[]
+): ValidationError[][] | null => {
+  console.log("validateProducts!!!!!!!!!!!", data);
+  let hasErrors = false;
+  const validationErrors = data.map((data) => {
+    const dtoInstance: ProductDto = new ProductDto(data);
+    const errors: ValidationError[] = validateSync(dtoInstance);
+    if (errors.length) hasErrors = true;
+    return errors;
+  });
+
+  return hasErrors ? validationErrors : null;
+};
+
+export const copyFileToParsedFolder = async (record) => {
+  const commandParameters = {
+    Bucket: record.s3.bucket.name,
+    CopySource: encodeURI(`${record.s3.bucket.name}/${record.s3.object.key}`),
+    Key: record.s3.object.key.replace("uploaded", "parsed"),
+  };
+  const command = new CopyObjectCommand(commandParameters);
+  return await s3Client.send(command);
+};
+
+export const deleteFromUploaded = async (record) => {
+  const commandParameters = {
+    Bucket: BUCKET_NAME,
+    Key: record.s3.object.key,
+  };
+  const command = new DeleteObjectCommand(commandParameters);
+  return await s3Client.send(command);
+};
---
+++ import-service/src/service/queue.service.ts
@@ -0,0 +1,18 @@
+import { SendMessageCommandOutput, SQS } from "@aws-sdk/client-sqs";
+import { ProductDto } from "../dto/product.dto";
+
+class QueueService {
+  private queueURL: string = process.env.SQS_URL;
+  private sqs: SQS = new SQS({ region: "eu-west-1" });
+
+  async addToQueue(product: ProductDto): Promise<SendMessageCommandOutput> {
+    console.log("addToQueue!!!!!!!!!!!!!!", product);
+    return await this.sqs.sendMessage({
+      QueueUrl: this.queueURL,
+      MessageBody: JSON.stringify(product),
+    });
+  }
+}
+
+export type { QueueService };
+export default new QueueService();
